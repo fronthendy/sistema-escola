54202e76a5af529f2fc743f074ae837dc808397a
Hendy Almeida <hendy.almeida@hotmail.com>
2021-07-14 22:05:19 -0300

    sequelize- dados dinamicos para view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 const alunoController = require('../controllers/alunoController');
+const indexController = require('../controllers/indexController');
 const professorController = require('../controllers/professorController');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 // http://localhost:3000/alunos
@@ -28,5 +31,21 @@ router.put('/professores/:id', professorController.update);
 
 // http://localhost:3000/professores/:id
 router.delete('/professores/:id', professorController.destroy);
+
+
+// http://localhost:3000/dashboard
+router.get('/dashboard', indexController.index);
+
+// http://localhost:3000/alunos/novo
+router.get('/alunos/novo', indexController.novo);
+
+// http://localhost:3000/alunos/novo
+router.post('/alunos/novo', indexController.create);
+
+// http://localhost:3000/alunos/:id
+router.get('/alunos/:id', indexController.editarAluno);
+
+router.post('/alunos/:id', indexController.atualizarAluno);
+
 
 module.exports = router;
